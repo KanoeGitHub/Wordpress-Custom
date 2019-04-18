db120b263945cc30ce000fb375a3684c606e68f6
Kanoe <estancia.k_0493@outlook.jp>
2019-04-18 22:05:58 +0900

    update

---
+++ custom.php
@@ -0,0 +1,59 @@
+<?php //通常ページとAMPページの切り分け
+/*
+Template Name:ライター一覧
+*/
+if ( !defined( 'ABSPATH' ) ) exit;
+
+if (!is_amp()) {
+	get_header();
+ } else {
+	get_template_part('tmp/amp-header');
+ }
+?>
+
+
+<?php //固定ページ内容
+get_template_part('tmp/page-contents'); ?>
+
+<!--投稿者一覧を表示-->
+<?php $users =get_users( array('orderby'=>ID,'order'=>ASC) );
+echo '<aside id="author_box-5" class="widget widget-content-top widget_author_box">';
+
+foreach($users as $user):
+	echo '<div class="author-box border-element no-icon cf">';
+	$uid = $user->ID;
+	$userData = get_userdata($uid);
+			echo '<figure class="author-thumb circle-image">';
+				echo get_avatar( $uid ,300 );
+			echo '</figure>';
+			echo '<div class="author-content">';
+				echo '<div class="author-name">'.$user->display_name.'</div>';
+				echo '<div class="author-description"><p>'.$userData->user_description.'</p></div>';
+
+			echo '<div class="author-follows">';
+        		echo '<div class="sns-follow bc-brand-color fbc-hide">';
+            		echo '<div class="sns-follow-message">'.$user->display_name.'をフォローする</div>';
+	    			echo '<div class="sns-follow-buttons sns-buttons">';
+						if($user->home_url != ""):
+						echo'<a href="'.$user->home_url.'" class="follow-button website-button website-follow-button-sq" target="_blank" title="著者サイト" rel="nofollow noopener noreferrer"><span class="icon-home-logo"></span></a>';
+						endif;
+						if($user->twitter_url != ""):
+						echo'<a href="'.$user->twitter_url.'" class="follow-button twitter-button twitter-follow-button-sq" target="_blank" title="Twitterをフォロー" rel="nofollow noopener noreferrer"><span class="icon-twitter-logo"></span></a>';
+						endif;
+						if($user->youtube_url != ""):
+						echo'<a href="'.$user->youtube_url.'" class="follow-button youtube-button youtube-follow-button-sq" target="_blank" title="youtubeをフォロー" rel="nofollow noopener noreferrer"><span class="icon-youtube-logo"></span></a>';
+						endif;
+						if($user->github_url != ""):
+						echo'<a href="'.$user->github_url.'" class="follow-button github-button github-follow-button-sq" target="_blank" title="githubをフォロー" rel="nofollow noopener noreferrer"><span class="icon-github-logo"></span></a>';
+						endif;
+					echo '</div>';
+				echo '</div>';
+			echo '</div>';
+			
+		echo '</div>';
+	echo '</div>';
+endforeach;
+echo '</aside>'; ?>
+
+
+<?php get_footer(); ?>
